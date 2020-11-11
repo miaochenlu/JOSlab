b056cf6c1d944b653f6bb6d542a6c826373f4ca1
miaochenlu <3170102668@zju.edu.cn>
2020-11-11 14:27:01 +0800

    add lab3 vmprint

--- lab3/xv6-labs-2020/kernel/vm.c
+++ lab3/xv6-labs-2020/kernel/vm.c
@@ -440,3 +440,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprintRecursive(pagetable_t pagetable, int level) {
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      // this PTE points to a lower-level page table.
+      for(int i = 0; i < level; i++) {
+        printf("..");
+        if(i != level - 1) printf(" ");
+      }
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0)
+        vmprintRecursive((pagetable_t)child, level + 1);
+    } 
+  }
+}
+
+void vmprint(pagetable_t pagetable) 
+{
+  printf("page table %p\n", pagetable);
+  vmprintRecursive(pagetable, 1);
+}
